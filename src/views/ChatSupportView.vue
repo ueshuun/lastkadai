5a3ec2da2c8b8e39f83fe162c593d01931a81b10
ueshuun <sueda@dl-techx-xedge.net>
2025-09-25 14:28:59 +0900

    fix: some changes

--- src/views/ChatSupportView.vue
+++ src/views/ChatSupportView.vue
@@ -37,7 +37,7 @@
           </h2>
           <v-card
             class="pa-3 mb-3"
-            height="80%"
+            height="60%"
             style="overflow-y:auto; border-radius: 12px; background: white;"
             ref="chatArea"
           >
